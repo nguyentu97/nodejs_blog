a6cd85fadc6b0b8fd3d9ab190c69d251d5cb7270
nguyen tu <tundk57@gmail.com>
2021-05-13 00:41:30 +0700

    structure foder, add template engine handlebars

---
+++ src/index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const path = require("path");
+const morgan = require("morgan");
+const handlebars = require("express-handlebars");
+const app = express();
+const port = 3000;
+
+// HTTP logger
+app.use(morgan("combined"));
+
+// template engine
+app.engine("hbs", handlebars({ extname: ".hbs" }));
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "resources/views"));
+console.log("path: ", path.join(__dirname, "resources/views"));
+app.get("/", (req, res) => {
+  res.render("home");
+  // res.send(` <h1 style="color:red";>Hello World123!</h1> `);
+});
+app.get("/news", (req, res) => {
+  res.render("news");
+  // res.send(` <h1 style="color:red";>Hello World123!</h1> `);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
