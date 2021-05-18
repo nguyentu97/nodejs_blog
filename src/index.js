712cd3aea7d6809e1f1ff9a33a6efcd36134b099
nguyen tu <tundk57@gmail.com>
2021-05-18 20:38:41 +0700

    config static file and scss

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ const handlebars = require("express-handlebars");
 const app = express();
 const port = 3000;
 
+app.use(express.static(path.join(__dirname, "public")));
+
 // HTTP logger
 app.use(morgan("combined"));
 
@@ -12,7 +14,7 @@ app.use(morgan("combined"));
 app.engine("hbs", handlebars({ extname: ".hbs" }));
 app.set("view engine", "hbs");
 app.set("views", path.join(__dirname, "resources/views"));
-console.log("path: ", path.join(__dirname, "resources/views"));
+console.log("path: ", path.join(__dirname, "public/img"));
 app.get("/", (req, res) => {
   res.render("home");
   // res.send(` <h1 style="color:red";>Hello World123!</h1> `);
